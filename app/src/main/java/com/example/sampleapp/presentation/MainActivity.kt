4a42541d095cda868225bd6f8efdf530d7622736
Lamer0 <combinerk@gmail.com>
2021-07-05 20:52:32 +0300

    +
    Signed-off-by: Lamer0 <combinerk@gmail.com>

--- app/src/main/java/com/example/sampleapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/presentation/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.sampleapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.sampleapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun loginClick(view: View) {
+
+
     }
 }
